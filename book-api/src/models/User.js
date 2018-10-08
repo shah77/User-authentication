e2cebc11bcdd670f563187251b31657ca74fec3c
haha7 <haha7@gmail.com>
2018-10-08 11:56:56 +0800

    user auth system

---
+++ book-api/src/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const schema = new mongoose.Schema(
+    {
+        email: {
+            type: String, 
+            required: true, 
+            lowercase: true, 
+            unique: true
+        },
+        passwordHash: { type: String, required: true},
+        confirmed: {type: Boolean, default: false}
+    },
+    {
+        timestamps: true
+    }
+);
+
+schema.methods.isValidPassword = function isValidPassword(password){
+    return bcrypt.compareSync(password, this.passwordHash); 
+}
+
+schema.methods.setPassword = function setPassword(password){
+    this.passwordHash = bcrypt.hashSync(password, 10);
+}
+
+schema.methods.generateJWT = function generateJWT(){
+    return jwt.sign(
+        {
+        email : this.email
+        },
+        process.env.JWT_SECRET
+    );
+}
+
+schema.methods.toAuthJSON = function toAuthJSON(){
+    return {
+        email : this.email,
+        confirmed : this.confirmed,
+        token : this.generateJWT()
+    };
+};
+
+schema.plugin(uniqueValidator, {message:'This email has been used by someone!'})
+
+export default mongoose.model('User', schema);
